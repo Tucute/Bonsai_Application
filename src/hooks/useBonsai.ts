06f85d1ee7dc3380ab76d2d594ba744feb606688
Tien-A <tienbluejay@gmail.com>
2024-01-11 11:29:39 +0700

    Tien/fix: update hook catalog

--- src/hooks/useBonsai.ts
+++ src/hooks/useBonsai.ts
@@ -1,9 +1,12 @@
 import {useState, useEffect} from 'react';
-const CATALOG = 'https://63a571e42a73744b008e23ee.mockapi.io/user24';
-const DATA_STORE = 'https://85a2-103-19-99-68.ngrok-free.app/api/get-products';
 import axios from 'axios';
 import {useQuery} from '@tanstack/react-query';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {url} from '../components/url/urlNgrok';
+
+const CATALOG = 'https://63a571e42a73744b008e23ee.mockapi.io/user24';
+const DATA_STORE = `${url}/api/get-product-admin`;
+const DATA_DEMO = 'https://63a571e42a73744b008e23ee.mockapi.io/users';
 
 export interface BonsaiType {
   id: string;
@@ -12,44 +15,24 @@ export interface BonsaiType {
   description: string;
   price: number | string;
   promotion_price: number | string;
+  category_id: number | string;
 }
 
 export interface CategoriesType {
+  id: string | number;
   name: string;
 }
-
 
 const useBonsai = () => {
   const [dataBonsai, setDataBonsai] = useState<BonsaiType[]>([]);
   const [dataCategories, setDataCategories] = useState<CategoriesType[]>([]);
 
-  const getUserID = async() => {
-    try {
-      const getUser = await AsyncStorage.getItem('user');
-      console.log("ID nguoi dung khi no dang nhap: ",getUser);
-        
-      
-      if (getUser) {
-        const user = JSON.parse(getUser);
-        const userID = user.id;
-        return userID;
-      } else {
-        console.error('User data not found in AsyncStorage');
-        return null;
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
   useEffect(() => {
     const fetchDataBonsai = async () => {
       try {
-        const res = await fetch(DATA_STORE);
+        const res = await fetch(DATA_DEMO);
         const data = await res.json();
-        const supplierID = await getUserID();
-        const bonsaiData = data.filter(item => item.supplier_id === 4)
-        setDataBonsai(bonsaiData);
+        setDataBonsai(data);
       } catch (error) {
         console.error(error);
       }
@@ -101,15 +84,14 @@ const useBonsai = () => {
     }
   };
 
-  const { data } = useQuery({
+  const {data} = useQuery({
     queryKey: ['dataCatalog'],
     queryFn: async () => {
       const res = await axios.get(CATALOG);
-      const catalogName = res.data.map((item: CategoriesType) => item.name);
-      return catalogName;
+      return res.data;
     },
   });
-  
+
   useEffect(() => {
     if (data) {
       setDataCategories(data);
